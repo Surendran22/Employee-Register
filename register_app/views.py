f29da719f5ca5fdf7288a9f866f13cb352e58d4a
Surendran22 <asurendhar49@gmail.com>
2022-07-12 21:53:58 +0530

    first commit

---
+++ register_app/views.py
@@ -0,0 +1,35 @@
+from xml.dom.minidom import Identified
+from django.shortcuts import redirect, render
+# from register_app.forms import EmployeeForm
+from .forms import EmployeeForm
+from .models import Employee
+
+# Create your views here.
+
+def employee_list(request):
+    context = {'employee_list':Employee.objects.all()}
+    return render(request,"register_app/employee_list.html",context)
+
+def employee_form(request,id=0):
+    if request.method == "GET":
+        if id==0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(instance=employee)    
+        return render(request,"register_app/employee_form.html",{'form':form})
+    else:
+        if id == 0:
+            form = EmployeeForm(request.POST)
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(request.POST,instance = employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
+
+def employee_delete(request,id):
+    employee = Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
+
